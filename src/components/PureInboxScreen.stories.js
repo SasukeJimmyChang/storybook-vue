89ecce850516f97b706db2ecdc3031277bb5e934
JimmyChang <whoelse1214@gmail.com>
2024-12-13 19:40:22 +0800

    Fix: Screens  WithInteractions errors

--- src/components/PureInboxScreen.stories.js
+++ src/components/PureInboxScreen.stories.js
@@ -1,5 +1,6 @@
 import PureInboxScreen from './PureInboxScreen.vue'
 
+import * as TaskStories from './Task.stories'
 import { fireEvent, within } from '@storybook/test'
 
 export default {
@@ -15,6 +16,17 @@ export const Error = {
 }
 
 export const WithInteractions = {
+  args: {
+    tasks: [
+      { ...TaskStories.Default.args.task, id: '1', title: 'Task 1 (pinned)', state: 'TASK_PINNED' },
+      { ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
+      { ...TaskStories.Default.args.task, id: '3', title: 'Task 3 (pinned)', state: 'TASK_PINNED' },
+      { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
+      { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
+      { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
+      { ...TaskStories.Default.args.task, id: '7', title: 'Task 7' }
+    ]
+  },
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement)
     // Simulates pinning the first task
